6546411c2c2fc5f348406950b5445e6d32fe3f20
graygill5 <ggill5@vols.utk.edu>
2025-07-23 11:15:23 -0400

    finally

---
+++ RUSH/RUSH/RUSHApp.swift
@@ -0,0 +1,17 @@
+//
+//  RUSHApp.swift
+//  RUSH
+//
+//  Created by Grayson Gill on 7/23/25.
+//
+
+import SwiftUI
+
+@main
+struct RUSHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RUSH/RUSHTests/RUSHTests.swift
@@ -0,0 +1,17 @@
+//
+//  RUSHTests.swift
+//  RUSHTests
+//
+//  Created by Grayson Gill on 7/23/25.
+//
+
+import Testing
+@testable import RUSH
+
+struct RUSHTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
